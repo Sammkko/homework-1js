165fd1ea8e5745da73081d2e945431c967830739
Sammkko <wolf.j2.sam@gmail.com>
2022-09-15 17:39:00 +0300

    First

---
+++ 6/js/app.js
@@ -0,0 +1,30 @@
+// function makeArmy() {
+
+//     let shooters = [];
+  
+//     for (let i = 0; i < 10; i++) {
+//       shooters.push(function() {
+//         alert( i ); 
+//       });
+//     }
+  
+//     return shooters;
+//   }
+  
+//   let army = makeArmy();
+  
+//   army[1](); 
+//   army[7]();
+
+
+const text = document.querySelector('.text');
+const title = document.querySelector('.title');
+
+text.onmouseenter = () => {
+    text.setAttribute('class', 'title');
+}
+
+text.onmouseleave = () =>{
+    text.classList.remove('title');
+    text.setAttribute('class', 'text');
+}
